0cd7165fd9374f9b3c117dcc06d401afc8b44324
lswitkowski <63552929+lswitkowski@users.noreply.github.com>
2024-09-15 13:48:03 -0700

    Build forecast functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 
 function refreshWeather(response) {
-  console.log(response.data);
+  getForecast(response.data.city);
   let realTemp = document.querySelector(".current-temperature-value");
   let currentTemp = Math.round(response.data.temperature.current);
   realTemp.innerHTML = currentTemp;
@@ -64,7 +64,37 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Lisbon");
+function getForecast(city){
+ let apiKey = "65ae2e8ao4f01409ca53644a9atfcbed";
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+ axios.get(apiUrl).then(displayForecast);
+}
+
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="future-forecast-day">
+
+ <div class="future-forecast-date">${day}</div>
+  <div class="future-forecast-icon">☀️ </div>
+  <div class="future-forecast-temps">
+    <div class="future-forecast-temp"><strong>5°</strong></div> 
+    <div class="future-forecast-temp">9°</div>
+  </div></div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+        displayForecast();
+
+        let searchFormElement = document.querySelector("#search-form");
+        searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+        searchCity("Lisbon");
+        
